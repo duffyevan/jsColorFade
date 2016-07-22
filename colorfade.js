0948d752e230192c6a46de5b3228e0ff81eb96d7
Evan Duffy <duffyevan@users.noreply.github.com>
2016-07-22 00:05:30 -0400

    Fixed js
    Removed debug lines and fixed indents. Also added fps speed variable

--- colorfade.js
+++ colorfade.js
@@ -1,29 +1,28 @@
+var fps = 100
 var r = 2;
-    var g = 252;
-    var b = 128;
-    var dr = 1;
-    var dg = 2;
-    var db = -1;
-    setInterval(frame, 30);
+var g = 252;
+var b = 128;
+var dr = 1;
+var dg = 2;
+var db = -1;
+setInterval(frame, 1000/fps);
 
-    
-    function frame(){
-        r+=dr;
-        g+=dg;
-        b+=db;
-
-        if (r == 254 || r == 0){
-            dr *= -1;
-            r += dr;
-        }
-        if (g == 254 || g == 0){
-            dg *= -1;
-            g += dg;
-        }
-        if (b == 254 || b == 0){
-            db *= -1;
-            b += db;
-        }
-        //document.write(r);
-        document.getElementById("animate").style = "background-color: rgb(" + r + ", " + g + ", " + b + ");"
+function frame(){
+    r+=dr;
+    g+=dg;
+    b+=db;
+    if (r == 254 || r == 0){
+        dr *= -1;
+        r += dr;
     }
+    if (g == 254 || g == 0){
+        dg *= -1;
+        g += dg;
+    }
+    if (b == 254 || b == 0){
+        db *= -1;
+        b += db;
+    }
+
+    document.getElementById("animate").style = "background-color: rgb(" + r + ", " + g + ", " + b + ");"
+}
